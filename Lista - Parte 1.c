79c550f901f9194dac14bbf40ef333d6424b3a67
unknown <victorvicmr@gmail.com>
2025-05-20 12:23:36 -0300

    1 - Inserção Básica

---
+++ Lista - Parte 1.c
@@ -0,0 +1,94 @@
+// Lista de Execícios: Listas Encadeadas, Filas e Pilhas
+
+// 1) Inserção Básica
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+
+    int dado;
+    struct no* proximo;
+} no;
+
+void inserir_inicio (no **lista, int valor) {
+
+    no* novo = (no*) malloc (sizeof (no));
+
+    if (novo == NULL) {
+
+        printf ("Erro ao tentar alocar memoria\n");
+        return;
+    }
+
+    novo -> dado = valor;
+    novo -> proximo = *lista;
+    *lista = novo;
+}
+
+void inserir_fim (no** lista, int valor) {
+
+    no* novo = (no*) malloc (sizeof (no));
+
+    if (novo == NULL) {
+
+        printf ("Erro ao tentar alocar memoria\n");
+        return;
+    }
+
+    novo -> dado = valor;
+    novo -> proximo = NULL;
+
+    if (*lista == NULL) {
+
+        *lista = novo;
+    }
+
+    else {
+
+        no* atual = *lista;
+
+        while (atual -> proximo != NULL) {
+
+            atual = atual -> proximo;
+        }
+
+        atual -> proximo = novo;
+    }
+}
+
+void imprimir_lista (no* lista) {
+
+    while (lista != NULL) {
+
+        printf ("%d -> ", lista -> dado);
+        lista = lista -> proximo;
+    }
+
+    printf ("NULL\n");
+}
+
+void liberar_memoria (no* lista) {
+
+    no* temp;
+
+    while (lista != NULL) {
+
+        temp = lista;
+        lista = lista -> proximo;
+        free (temp);
+    }
+}
+
+int main () {
+
+    no* lista = NULL;
+
+    inserir_fim (&lista, 10);
+    inserir_fim (&lista, 20);
+    inserir_inicio (&lista, 40);
+    inserir_inicio (&lista, 60);
+
+    imprimir_lista (lista);
+    liberar_memoria (lista);
+}
